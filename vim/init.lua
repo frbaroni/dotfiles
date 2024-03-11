f066f7dc9200cad6705a8ebb36b66fb1145e9fab
Fernando Baroni <f@baroni.tech>
2024-03-11 13:22:03 -0300

    Update mason/lspzero

--- vim/init.lua
+++ vim/init.lua
@@ -71,24 +71,49 @@ require('packer').startup(function(use)
 			{ 'hrsh7th/nvim-cmp' },
 			{ 'hrsh7th/cmp-buffer' },
 			{ 'hrsh7th/cmp-path' },
-			{ 'saadparwaiz1/cmp_luasnip' },
 			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'hrsh7th/cmp-nvim-lua' },
-
-			-- Snippets
-			{ 'L3MON4D3/LuaSnip' },
-			{ 'rafamadriz/friendly-snippets' },
 		},
 		config = function()
+			local lsp_zero = require('lsp-zero')
+
+			lsp_zero.on_attach(function(client, bufnr)
+				-- see :help lsp-zero-keybindings
+				-- to learn the available actions
+				lsp_zero.default_keymaps({buffer = bufnr})
+			end)
+
+			--- if you want to know more about lsp-zero and mason.nvim
+			--- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+			require('mason').setup({})
+			require('mason-lspconfig').setup({
+				ensure_installed = {},
+				handlers = {
+					lsp_zero.default_setup,
+					lua_ls = function()
+						local lua_opts = lsp_zero.nvim_lua_ls()
+						require('lspconfig').lua_ls.setup(lua_opts)
+					end,
+				}
+			})
+
 			local cmp = require('cmp')
+			local cmp_format = lsp_zero.cmp_format()
 
 			cmp.setup({
+				formatting = cmp_format,
 				mapping = cmp.mapping.preset.insert({
-					-- Scroll up and down in the completion documentation
+					-- scroll up and down the documentation window
 					['<C-u>'] = cmp.mapping.scroll_docs(-4),
 					['<C-d>'] = cmp.mapping.scroll_docs(4),
-				})
+				}),
 			})
+--			lspconfig.biome.setup {
+--				root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")
+--			}
+--
+--			lspconfig.tsserver.setup {
+--				root_dir = lspconfig.util.root_pattern("package.json"),
+--			}
 		end
 	}
 
@@ -282,45 +307,6 @@ wk.register({
 	['K'] = { vim.lsp.buf.hover, 'Show Hover' },
 	['<C-space>'] = { "<C-\\><C-n>", 'Exit Terminal mode', mode = 't' },
 }, {})
-
-local lsp = require('lsp-zero')
-local lspconfig = require('lspconfig')
-
-lsp.preset('recommended')
-
-lspconfig.lua_ls.setup {
-	settings = {
-		Lua = {
-			runtime = {
-				version = 'LuaJIT',
-			},
-			diagnostics = {
-				globals = { 'vim' },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
-}
-
-lspconfig.biome.setup {
-	root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")
-}
-
-lspconfig.tsserver.setup {
-	root_dir = lspconfig.util.root_pattern("package.json"),
-	single_file_support = false
-}
-
-vim.diagnostic.config({
-	virtual_text = true,
-})
-
-lsp.setup()
 
 -- Triger `autoread` when files changes on disk
 -- https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
